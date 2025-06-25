747bd33d5bcc2216d36cc31ab2e532bbb2c848d2
nailuJ-dev <julian.buzare@gmail.com>
2025-06-25 14:02:30 +0000

    étape 9

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -28,9 +28,23 @@ class UserController
     
     public function checkCreate(): void
     {
-        // To be determined later, waiting for the specific logic
-        // Our own logic or a determined logic gives in the next steps
-        // Display a message after checking datas with id perhaps if id is set then...
+        if (!empty($_POST))
+        {
+            $email = htmlspecialChars($_POST['email']);
+            $firstName = htmlspecialChars($_POST['first_name']);
+            $lastName = htmlspecialChars($_POST['last_name']);
+            
+            $newUser = new User();
+            $newUser->setEmail($email);
+            $newUser->setFirstName($firstName);
+            $newUser->setLastName($lastName);
+            
+            $userManager = new UserManager();
+            $userManager->create($newUser);
+            
+            header('Location: index.php');
+            exit();
+        }
     }
     
     public function checkUpdate(): void
